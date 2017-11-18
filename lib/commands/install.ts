2d423fa805f4545d416e99c7235788155039329c
Jos Kuijpers <jkuijpers@jarvix.nl>
2017-11-18 11:58:03 +0100

    Improve install command infO

--- lib/commands/install.ts
+++ lib/commands/install.ts
@@ -14,10 +14,14 @@ export default class InstallCommand extends Command {
 
         /*
 
-        - If needed, build
+        - If needed, build:
+          - if zip does not exist
+          - if zip does exist but the files that matter are older
+
         - Find mods folder of installation, or use config
 
         - Copy ZIP to that folder
+          - not update, so find local ZIP file. if it does not exist, do build.
 
          */
     }
